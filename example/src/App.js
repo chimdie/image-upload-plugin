d4e054dbb4f0fa5901d7f32ca0bb99cf8382a55f
chimdie <nzechimdi@yahoo.com>
2021-08-25 13:02:51 +0100

    adds deletion button

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
 import UploadLoader from 'image-upload-plugin'
-// import 'image-upload-plugin/dist/index.css'
 
 const App = () => {
   function getImageFileObject(imageFile) {
-    console.log({ imageFile })
+    console.log({imageFile})
   }
   return <UploadLoader onFileAdded={(img) => getImageFileObject(img)} />
 }
